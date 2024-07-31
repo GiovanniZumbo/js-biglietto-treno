0c969a3a8eae95945805b531b97954d8f1f09cf3
Giovanni Zumbo <gianni.zu07@gmail.com>
2024-07-31 16:18:03 +0200

    RE-ordering

--- script.js
+++ script.js
@@ -5,11 +5,11 @@
 
 /*
 Creare una variabile per il costo per chilometro / Make a price/distance var
+Creare una variabile per lo sconto minorenni / Make an under18 discount var
+Creare una variabile per lo sconto over 65 / Make an over65 discount var
 Chiedere il numero di chilometri da percorrere. / Request travel distance 
 Chiedere l'età del passeggero. / Request traveller age
 Calcolare il prezzo del biglietto senza sconto. / Calculate full price ticket
-Creare una variabile per lo sconto minorenni / Make an under18 discount var
-Creare una variabile per lo sconto over 65 / Make an over65 discount var
 Calcolare il prezzo finale del biglietto. / Calculate reduced price ticket
 Stampare il risultato / Output the result
 
@@ -17,40 +17,39 @@ Stampare il risultato / Output the result
 
 // # STAGING
 
-// * 0. Creare una variabile per il costo per chilometro / Make a price/distance var
+// * 1. Creare una variabile per il costo per chilometro / Make a price/distance var
 const priceUnit = 0.21;
+
+// * 2. Creare una variabile per lo sconto minorenni / Make an under18 discount var 
+const underPercDiscount = 20;
+
+// * 3. Creare una variabile per lo sconto over 65 / Make an over65 discount var
+const overPercDiscount = 40;
 
 // # GATHERING DATA
 
-// * 1. Chiedere il numero di chilometri da percorrere. / Request travel distance 
+// * 4. Chiedere il numero di chilometri da percorrere. / Request travel distance 
 
 const distance = parseInt(prompt("Inserisci la distanza da percorrere (km)", 12, 5))
 console.log('distance', distance);
 
-// * 2. Chiedere l'età del passeggero. / Request traveller age
+// * 5. Chiedere l'età del passeggero. / Request traveller age
 
-const age = prompt("Inserisci la tua età", 25);
+const age = parseInt(prompt("Inserisci la tua età", 25));
 console.log('age', age);
 
 // # PROCESSING
 
-// * 3. Calcolare il prezzo del biglietto senza sconto. / Calculate full price ticket
+// * 6. Calcolare il prezzo del biglietto senza sconto. / Calculate full price ticket
 
 const ticketPrice = ((distance * priceUnit).toFixed(2));
 console.log('ticketPrice', ticketPrice);
 
-// * 4. Creare una variabile per lo sconto minorenni / Make an under18 discount var 
 
-
-const underPercDiscount = 20;
-
-// * 5. Creare una variabile per lo sconto over 65 / Make an over65 discount var
-
-const overPercDiscount = 40;
-
-// * 6. Calcolare il prezzo finale del biglietto. / Calculate reduced price ticket
+// * 7. Calcolare il prezzo finale del biglietto. / Calculate reduced price ticket
 
 // Under
+
 function underPrice(ticketPrice, underPercDiscount) {
     const underDiscount = (ticketPrice * underPercDiscount) / 100;
     return ticketPrice - underDiscount;
@@ -67,20 +66,18 @@ function overPrice(ticketPrice, overPercDiscount) {
 
 const finalOverPrice = overPrice(ticketPrice, overPercDiscount).toFixed(2)
 
-
 // # OUTPUT 
 
-// * 7. Stampare il risultato / Output the result
+// * 8. Stampare il risultato / Output the result
 
 if (age < 18) {
-    alert(`Il costo del tuo biglietto è: ${finalUnderPrice}€`);
+    alert(`Il costo del tuo biglietto è: ${finalUnderPrice}€. Hai usufruito del 20% di sconto.`);
     console.log('finalUnderPrice', finalUnderPrice);
 
 } else if (age >= 65) {
-    alert(`Il costo del tuo biglietto è: ${finalOverPrice}€`);
+    alert(`Il costo del tuo biglietto è: ${finalOverPrice}€. Hai usufruito del 40% di sconto.`);
     console.log('finalOverPrice', finalOverPrice);
 
 } else {
     alert(`Il costo del tuo biglietto è: ${ticketPrice}€`);
-    console.log('ticketPrice', ticketPrice)
 }
