f455cb3fc8fb8a1686841f551c4f621ac4603e66
Giovanni Zumbo <gianni.zu07@gmail.com>
2024-07-31 15:00:06 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,29 @@
+// @ Il programma dovrà chiedere all'utente il numero di chilometri che vuole percorrere e l'età del passeggero. Sulla base di queste informazioni dovrà calcolare il prezzo totale del viaggio.
+
+
+// PLANNING
+
+/*
+
+Chiedere il numero di chilometri da percorrere. / Request travel distance 
+Chiedere l'età del passeggero. / Request traveller age
+Calcolare il prezzo del biglietto senza sconto. / Calculate full price ticket
+Creare una variabile per lo sconto minorenni / Make a under18 discount var
+Creare una variabile per lo sconto over 65 / Make a over65 discount var
+Calcolare il prezzo finale del biglietto. / Calculate reduced price ticket
+Stampare il risultato / Output the result
+
+*/
+
+// # STAGING
+
+// # GATHERING DATA
+
+// * 1. Chiedere il numero di chilometri da percorrere. / Request travel distance 
+
+const distance = parseInt(prompt("Inserisci la distanza da percorrere (km)", 0))
+console.log('distance', distance);
+
+// # PROCESSING
+
+// # OUTPUT 
