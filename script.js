fbb036ee9ef35f28950f75b41769818b5d58ea53
Giovanni Zumbo <gianni.zu07@gmail.com>
2024-07-31 16:02:34 +0200

    Added output

--- script.js
+++ script.js
@@ -8,8 +8,8 @@ Creare una variabile per il costo per chilometro / Make a price/distance var
 Chiedere il numero di chilometri da percorrere. / Request travel distance 
 Chiedere l'età del passeggero. / Request traveller age
 Calcolare il prezzo del biglietto senza sconto. / Calculate full price ticket
-Creare una variabile per lo sconto minorenni / Make a under18 discount var
-Creare una variabile per lo sconto over 65 / Make a over65 discount var
+Creare una variabile per lo sconto minorenni / Make an under18 discount var
+Creare una variabile per lo sconto over 65 / Make an over65 discount var
 Calcolare il prezzo finale del biglietto. / Calculate reduced price ticket
 Stampare il risultato / Output the result
 
@@ -24,12 +24,12 @@ const priceUnit = 0.21;
 
 // * 1. Chiedere il numero di chilometri da percorrere. / Request travel distance 
 
-const distance = parseInt(prompt("Inserisci la distanza da percorrere (km)", 0))
+const distance = parseInt(prompt("Inserisci la distanza da percorrere (km)", 12, 5))
 console.log('distance', distance);
 
 // * 2. Chiedere l'età del passeggero. / Request traveller age
 
-const age = prompt("Inserisci la tua età");
+const age = prompt("Inserisci la tua età", 25);
 console.log('age', age);
 
 // # PROCESSING
@@ -39,6 +39,39 @@ console.log('age', age);
 const ticketPrice = ((distance * priceUnit).toFixed(2));
 console.log('ticketPrice', ticketPrice);
 
-alert(`Il costo del tuo biglietto è: ${ticketPrice}€`)
+// * 4. Creare una variabile per lo sconto minorenni / Make an under18 discount var
+
+const underPercDiscount = 20;
+
+function underPrice(ticketPrice, underPercDiscount) {
+    const underDiscount = (ticketPrice * underPercDiscount) / 100;
+    return ticketPrice - underDiscount;
+}
+
+const finalUnderPrice = underPrice(ticketPrice, underPercDiscount).toFixed(2)
+
+// * 5. Creare una variabile per lo sconto over 65 / Make an over65 discount var
+
+const overPercDiscount = 40;
+
+function overPrice(ticketPrice, overPercDiscount) {
+    const overDiscount = (ticketPrice * overPercDiscount) / 100;
+    return ticketPrice - overDiscount;
+}
+
+const finalOverPrice = overPrice(ticketPrice, overPercDiscount).toFixed(2)
 
 // # OUTPUT 
+
+if (age < 18) {
+    alert(`Il costo del tuo biglietto è: ${finalUnderPrice}€`);
+    console.log('finalUnderPrice', finalUnderPrice);
+
+} else if (age >= 65) {
+    alert(`Il costo del tuo biglietto è: ${finalOverPrice}€`);
+    console.log('finalOverPrice', finalOverPrice);
+
+} else {
+    alert(`Il costo del tuo biglietto è: ${ticketPrice}€`);
+    console.log('ticketPrice', ticketPrice)
+}
